db1890950f7759bd6042772b7eda7344c047007b
wjl518 <wangjiulin_518@163.com>
2022-10-16 15:19:15 +0000

    update src/main/java/com/atguigu/git/GitTest.java.
    Signed-off-by: wjl518 <wangjiulin_518@163.com>

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("gitee test!");
+         System.out.println("gitee test2!");
     }
 }
